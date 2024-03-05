73610a8ef7a7a1982b577793cffa91d263b7eaf5
Claire <claire.simonot@hotmail.com>
2024-03-05 15:48:55 +0100

    0503

--- app.js
+++ app.js
@@ -10,19 +10,33 @@ app.use(express.urlencoded({ extended: false }));
 // permet la récupération des champs post
 app.use(express.json());
 // permet l'échange de données au format JSON
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 app.set("views", __dirname + "/view");
 app.set("view engine", "ejs");
+app.use(session({ secret: "Shh, its a secret!", cookie: { maxAge: 60000 } })); // maxage c'est la durée de ta session
 
-app.get("/", (req, res) => {
-  res.send("Bienvenue");
+function checkSignIn(req,res,next) {
+  if (req.session.user) {
+    console.log(req.session.user);
+    next()
+  } else {
+    var err = new Error("Not logged in!");
+    console.log(err);
+    process.exit();
+  }
+}
+
+app.get("/accueil", (req, res) => {
+  res.render("accueil");
 });
 app.get("/acteur/:id", (req, res) => {
   bdd.getActeur(req.params.id, function (row) {
     res.render("acteur", { acteur: row });
   });
 });
-app.get("/acteurs", (req, res) => {
+app.get("/acteurs", checkSignIn, (req, res) => {
   bdd.getAllActor(function (row) {
     res.render("acteurs", { acteurs: row });
   });
@@ -50,36 +64,63 @@ app.post("/send", (req, res) => {
   });
 });
 app.get("/modifier_acteur/:id", (req, res) => {
-      bdd.getActeur(req.params.id, function (row) {
-         res.render("modifier_acteur", { acteurs: row });
- });
+  bdd.getActeur(req.params.id, function (row) {
+    res.render("modifier_acteur", { acteurs: row });
+  });
 });
 app.post("/modifier_acteur", (req, res) => {
-
   bdd.ModifActor(req.body, function (r) {
-     if (r) {
-       res.redirect("/acteurs");
+    if (r) {
+      res.redirect("/acteurs");
     } else {
       res.send("err");
     }
-   });
+  });
 });
 app.get("/delete_acteur/:id", (req, res) => {
   bdd.getActeur(req.params.id, function (row) {
-     res.render("delete_acteur", { acteurs: row });
-});
+    res.render("delete_acteur", { acteurs: row });
+  });
 });
 app.post("/delete_acteur", (req, res) => {
-
-bdd.DeleteActor(req.body, function (r) {
- if (r) {
-   res.redirect("/acteurs");
-} else {
-  res.send("err");
-}
-});
+  bdd.DeleteActor(req.body, function (r) {
+    if (r) {
+      res.redirect("/acteurs");
+    } else {
+      res.send("err");
+    }
+  });
 });
 
 app.listen(port, () => {
   console.log("listen to port" + port);
+});
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+app.use(cookieParser());
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+app.post("/send_add_user", (req, res) => {
+  bdd.AddUser(req.body, function (r) {
+    if (r) {
+      res.redirect("/accueil");
+    } else {
+      res.send("err");
+    }
+  });
+});
+
+app.post("/connect", (req, res) => {
+  bdd.LogIn(req.body, function (r) {
+    if (r) {
+      req.session.user = r;
+      res.redirect("/acteurs");
+    } else {
+      res.send("err");
+    }
+  });
 });
--- model/pool.js
+++ model/pool.js
@@ -1,4 +1,6 @@
 const connect = require("./mysqlconfig.js");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 exports.getActeur = function (idacteur, cb) {
   connect.query(
@@ -28,9 +30,9 @@ exports.getAllRealisateur = function (cb) {
 };
 
 exports.AddActor = function (POST, cb) {
-   
   connect.query(
-    "INSERT INTO acteurs  (nom , prenom, photo) VALUES (?,?,?);", [POST.nom, POST.prenom, POST.photo],
+    "INSERT INTO acteurs  (nom , prenom, photo) VALUES (?,?,?);",
+    [POST.nom, POST.prenom, POST.photo],
     function (err) {
       if (err) cb(false);
       cb(true); // callback est une fonction envoyée en paramètres  à partir de app.js
@@ -38,10 +40,10 @@ exports.AddActor = function (POST, cb) {
   );
 };
 exports.DeleteActor = function (POST, cb) {
-   
   connect.query(
-    "DELETE  FROM acteurs  WHERE acteurs.id = ? ;",[POST.id],
-    
+    "DELETE  FROM acteurs  WHERE acteurs.id = ? ;",
+    [POST.id],
+
     function (err) {
       if (err) cb(false);
       cb(true); // callback est une fonction envoyée en paramètres  à partir de app.js
@@ -50,12 +52,39 @@ exports.DeleteActor = function (POST, cb) {
 };
 
 exports.ModifActor = function (POST, cb) {
-   
   connect.query(
-    "UPDATE acteurs  SET nom = ?, prenom = ?, photo = ? WHERE acteurs.id = ?;", [POST.nom, POST.prenom, POST.photo, POST.id],
-     function (err) {
-       if (err) cb(false);
-       cb(true); // callback est une fonction envoyée en paramètres  à partir de app.js
-     }
+    "UPDATE acteurs  SET nom = ?, prenom = ?, photo = ? WHERE acteurs.id = ?;",
+    [POST.nom, POST.prenom, POST.photo, POST.id],
+    function (err) {
+      if (err) cb(false);
+      cb(true); // callback est une fonction envoyée en paramètres  à partir de app.js
+    }
+  );
+};
+exports.AddUser = function (POST, cb) {
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    bcrypt.hash(POST.password, salt, function (err, hash) {
+      connect.query(
+        "INSERT INTO user  (email , password) VALUES (?,?);",
+        [POST.email, hash],
+        function (err) {
+          if (err) cb(false);
+          cb(true); // callback est une fonction envoyée en paramètres  à partir de app.js
+        }
+      );
+    });
+  });
+};
+exports.LogIn = function (POST, cb) {
+  connect.query(
+    " SELECT * FROM user WHERE email = ?;",
+    [POST.email],
+    function (err, rows) {
+    
+      bcrypt.compare(POST.password, rows[0].password, function (err,result) {
+        if (err) cb(false)
+        else { cb(rows[0]);} 
+      });
+    }
   );
 };
